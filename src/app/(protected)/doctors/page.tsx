3618d52330bb60d28f1011ef493e2fdb43196469
ClevertonZarelli <clevertonzarelli@hotmail.com>
2025-05-28 22:02:14 -0300

    fix: Create page components

---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,40 @@
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+
+const DoctorsPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar Médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-lg font-medium">Médicos</h2>
+          </div>
+        </div>
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default DoctorsPage;
