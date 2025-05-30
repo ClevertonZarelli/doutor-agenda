6206dd03fdd73dc06e1527070d9f63c156e2920d
ClevertonZarelli <clevertonzarelli@hotmail.com>
2025-05-29 22:20:17 -0300

    fix: calendario em ptBR

--- src/app/(protected)/appointments/_components/add-appointment-form.tsx
+++ src/app/(protected)/appointments/_components/add-appointment-form.tsx
@@ -289,7 +289,7 @@ const AddAppointmentForm = ({
                       disabled={(date) =>
                         date < new Date() || !isDateAvailable(date)
                       }
-                      initialFocus
+                      locale={ptBR}
                     />
                   </PopoverContent>
                 </Popover>
